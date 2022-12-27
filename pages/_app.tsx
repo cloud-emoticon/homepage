045c443318ea14b274605cba2e2a0a30d558be5a
KTachibanaM <whj19931115@gmail.com>
2022-12-27 01:36:56 -0800

    skeleton with mui and next

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,29 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import React from 'react';
+import {CacheProvider} from '@emotion/react';
+import {ThemeProvider, CssBaseline} from '@mui/material';
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
-}
+import createEmotionCache from '../utils/createEmotionCache';
+import lightTheme from '../styles/theme/lightTheme';
+import '../styles/globals.css';
+import Container from "@mui/material/Container";
+
+const clientSideEmotionCache = createEmotionCache();
+
+const MyApp = (props: any) => {
+  const {Component, emotionCache = clientSideEmotionCache, pageProps} = props;
+
+  return (
+      <CacheProvider value={emotionCache}>
+        <ThemeProvider theme={lightTheme}>
+          <CssBaseline/>
+          <Container sx={{
+            mt: 4
+          }}>
+            <Component {...pageProps} />
+          </Container>
+        </ThemeProvider>
+      </CacheProvider>
+  );
+};
+
+export default MyApp;
