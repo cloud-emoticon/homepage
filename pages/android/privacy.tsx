fc700d2b0dfa7ff51f55e3dd3268b175025b49ec
KTachibanaM <whj19931115@gmail.com>
2023-09-30 01:21:31 -0700

    fix build

--- pages/android/privacy.tsx
+++ pages/android/privacy.tsx
@@ -8,11 +8,11 @@ export default function AndroidPrivacy() {
                 <title>Privacy Policy for Cloud Emoticon Android App</title>
             </Head>
             <h1>Privacy Policy for Cloud Emoticon Android App</h1>
-            <p>1. If you enable the "quick enter" feature of the Android app, the app will process all texts you enter on your Android device.</p>
-            <p>2. The texts are used to solely provide the "quick enter" feature.</p>
+            <p>1. If you enable the &quot;quick enter&quot; feature of the Android app, the app will process all texts you enter on your Android device.</p>
+            <p>2. The texts are used to solely provide the &quot;quick enter&quot; feature.</p>
             <p>3. The texts are NEVER transmitted outside of your device.</p>
             <p>4. The texts are NEVER transmitted to any third-party service.</p>
-            <p>5. You can disable the "quick enter" feature at any time and the app will stop processing texts you enter immediately.</p>
+            <p>5. You can disable the &quot;quick enter&quot; feature at any time and the app will stop processing texts you enter immediately.</p>
             <p>6. The app will notify you if there is a change in this privacy policy.</p>
             <p>7. Email admin at ktachibana.party for any privacy questions or concerns.</p>
         </>
